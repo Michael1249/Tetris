9facbd67b0e86586bc855932cf61f144e946b180
Michael1249 <44965412+Michael1249@users.noreply.github.com>
2019-10-13 11:06:26 +0300

    add empty project with class interfaces

---
+++ Assets/Scripts/GameField.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameField : MonoBehaviour
+{
+    [SerializeField]
+    private GameController controller;
+
+    [SerializeField]
+    private Vector2Int field_size;
+
+    private Cell[,] field;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        field = new Cell[field_size.x, field_size.y];
+    }
+
+    // it'll call before tetramino move
+    public void clearCells()
+    {
+        // clear tetramino's cells from field
+    }
+
+    // it'll call after tetramino move
+    public void addCells()
+    {
+        // add tetramino's cells from field
+        // call represent update
+    }
+
+    public bool isTetraminoColide()
+    {
+        // check tetramino is colide with field
+        return false;
+    }
+
+    public bool isTetraminoStuck()
+    {
+        //check if tetramino is stucked and can't move down
+        return false;
+    }
+
+    public void RemoveFullLines()
+    {
+        // remove all full lines 
+    }
+
+    public Vector2Int getFieldSize()
+    {
+        return field_size;
+    }
+
+    public Cell[,] getField()
+    {
+        return field;
+    }
+}
---
+++ Assets/Scripts/VisualRepresent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualRepresent : MonoBehaviour
+{
+    [SerializeField]
+    private GameController controller;
+
+    void Start()
+    {
+
+    }
+
+    // it'll be called by GameLogic when field is updated
+    void UpdateRepresent()
+    {
+        // with field.getField() and field.getFieldSize() update represent
+    }
+}
