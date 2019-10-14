50eea8f51f0598521b6c46782f82cb56d8da2bdf
Gnome <alexeynshah@gmail.com>
2019-10-15 01:04:46 +0300

    InputButtons And PauseMenu

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    public bool gameIsPaused = false;
+
+    public void PauseGame()
+    {
+        Debug.Log("Pause");
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Debug.Log("Resume");
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void ExitToMainMenu()
+    {
+        Debug.Log("ExitToMainMenu");
+        SceneManager.LoadScene("MenuScene");
+    }
+}
